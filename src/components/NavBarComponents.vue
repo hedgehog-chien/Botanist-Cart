52f4c8b10466b1346d0a2413f4be20f8c962b946
Hedgehog Chien <j830813@gmail.com>
2022-03-18 14:49:49 +0800

    First Upload

---
+++ src/components/NavBarComponents.vue
@@ -0,0 +1,46 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark shadow-5-strong">
+                       <div class="container-fluid">
+                           <router-link to="/"><h1>Botanist</h1></router-link>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-lg-0 ">
+                        <li class="nav-item">
+                         <router-link class="nav-link active" aria-current="page" to="/">首頁</router-link>
+                        </li>
+                        <router-link class="nav-link" to="/signup">會員註冊</router-link>
+                        <li/>
+                        <li/>
+                        <router-link class="nav-link" to="/about">關於我們</router-link>
+                        <li/>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              商品一覽
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                              <a class="dropdown-item" href="#">開運植物</a>
+                              <a class="dropdown-item" href="#">開運花卉</a>
+                              <div class="dropdown-divider"></div>
+                              <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                          </li>
+                        <li class="nav-item">
+                        <a class="nav-link" href="cart.html"><i class="fas fa-cart-plus"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#"><i class="fa-solid fa-heart"></i></a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </nav>
+
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import SignupView from '../views/SignupView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignupView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
